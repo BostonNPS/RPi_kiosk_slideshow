9da167627151343dfa295963eee5e12a7a4b8172
Eric Hanson Plass <emh5004@gmail.com>
2016-10-14 17:16:10 -0400

    video placeholder pages
    placeholder pages before and after omxplayer covers it up with video.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var requestURL = 'https://script.google.com/macros/s/AKfycbw-uVY9vpvLd1wJN1XpS5QRxOgHsHe2EvGrqTKSpNT89JnfwFA/exec' //Faneuil Hall
 //var requestURL = 'https://script.google.com/macros/s/AKfycbz7CLS81nolWf_aXchoUrHb6BtRUKrCo_-AfIbzED1XBy_LXhE/exec' //CNY
 
-var usbAutoDir = '/media/pi/'
+var usbAutoDir = '/media/pi/809E-1BF2'
+var videoStart = 'video.html' //effectively a blank page that could have an icon, etc. that appears before video plays and after video quits.
+var videoEnd = 'video.html'
+var videoDelay = 2000;
 
 var express = require('express'),
     http = require('http');
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-//var exec = require('child_process').exec;
+var exec = require('child_process').exec;
 
 server.listen(80);
 var request = require('request');
@@ -89,6 +92,21 @@ app.get('/usb/:name', function (req, res) {
   };
 
   var fileName = req.params.name;
+  
+  if(fileName.search(".mp4" || ".mov") > -1) //if a movie, use omxplayer.
+  {
+	io.emit('currentSlide', "http://localhost/video/start");
+	setTimeout(function(){
+		exec("DISPLAY=:0 omxplayer " + usbAutoDir + "/" + fileName, function(error, stdout, stderr){
+			if(error){
+				console.log(error);
+			}
+			io.emit('currentSlide', "http://localhost/video/stop")
+		})
+	},videoDelay)
+  }
+  else
+  {
   res.sendFile(fileName, options, function (err) {
     if (err) {
       console.log(err);
@@ -98,13 +116,25 @@ app.get('/usb/:name', function (req, res) {
       console.log('Sent:', fileName);
     }
   });
+  }
 });
 
 app.get('/index.html', function (req, res) {
 	request(currentSlideURL, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			res.sendFile("slide_manager.html")
+			res.sendFile("slide_manager.html",{root:__dirname})
   		}
 	});
 });
 
+app.get('/video/:state', function (req, res) {
+	if(req.params.state == "stop" )
+	{
+			res.sendFile(videoEnd,{root:__dirname})
+	}
+	else
+	{
+		res.sendFile(videoStart,{root:__dirname})
+	}
+	
+});
